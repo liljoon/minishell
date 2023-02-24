4bf9d254fda5089c41a03fe04b1ccc7fca397efb
sunwooiljoon <wnsdl1659@naver.com>
2023-02-24 19:59:45 +0900

    feature : heredoc - not tested

--- redirections.c
+++ redirections.c
@@ -6,7 +6,7 @@
 /*   By: isunwoo <isunwoo@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 12:11:58 by isunwoo           #+#    #+#             */
-/*   Updated: 2023/02/21 16:51:28 by isunwoo          ###   ########.fr       */
+/*   Updated: 2023/02/24 19:55:01 by isunwoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,30 @@ void	redirection_append(char *file)
 	dup2(fd, 1);
 }
 
+void	redirection_heredoc(char *arg)
+{
+	int		temp_fd;
+	int		fd;
+	char	*command;
+
+	temp_fd = open("./.heredoc_temp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	while (1)
+	{
+		command = readline("> ");
+		if (!command)
+			break ;
+		else if (ft_strncmp(arg, command, ft_strlen(arg) == 0))
+		{
+			free(command);
+			break ;
+		}
+		write(temp_fd, command, ft_strlen(command));
+		free(command);
+	}
+	fd = open("./.heredoc_temp", O_RDONLY);
+	dup2(fd, 0);
+}
+
 char	*check_redirection_output(char *command)
 {
 	char	**str_arr;
@@ -49,7 +73,6 @@ char	*check_redirection_output(char *command)
 	dup2(fd, 1);
 	return (str_arr[0]);
 }
-
 
 char	*check_redirections(char *command)
 {
