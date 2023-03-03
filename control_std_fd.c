95592826f38e512a9530017ead227f4e03fef754
Iljoon Sunwoo <isunwoo@c6r10s6.42seoul.kr>
2023-03-03 17:51:49 +0900

    feature : control STDIN STDOUT

---
+++ control_std_fd.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   control_std_fd.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isunwoo <isunwoo@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/03 16:59:27 by isunwoo           #+#    #+#             */
+/*   Updated: 2023/03/03 17:20:23 by isunwoo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	copy_std_fd(int fd[])
+{
+	fd[0] = dup(0);
+	fd[1] = dup(1);
+}
+
+void	restore_std_fd(int fd[])
+{
+	dup2(fd[0], 0);
+	dup2(fd[1], 1);
+	close(fd[0]);
+	close(fd[1]);
+	unlink("./.heredoc_temp");
+}
