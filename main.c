67fbfd9a6817219abc61ab904b135b6e41a5c445
함윤주 <ham@ham-yunjuui-MacBookAir.local>
2023-03-05 18:14:44 +0900

    chore: fix typo

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ int	main(int argc, char *argv[], char *envp[])
 		}
 		if (command && *command)
 			add_history(command);
-		// tokenize(tks, command, 0);
+		// tokenize(tks, command);
 		if (!exec_builtins(tks))
 			exec_command(command, envp);
 		free(command);
