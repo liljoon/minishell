43ea3ed784ea104ffb6a65e1393eb177765d95ff
YoonJu Ham <judyham10634@naver.com>
2023-03-06 11:50:02 +0900

    feat: count total args in command (including operators)

--- parsing.c
+++ parsing.c
@@ -10,60 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
-// #include<stdlib.h>
+// #include "minishell.h"
+#include<stdlib.h>
 
-// size_t	ft_strlen(const char *s)
-// {
-// 	size_t	idx;
+size_t	ft_strlen(const char *s)
+{
+	size_t	idx;
 
-// 	idx = 0;
-// 	while (s[idx] != '\0')
-// 		idx++;
-// 	return (idx);
-// }
-// char	*ft_substr(char const *s, unsigned int start, size_t len)
-// {
-// 	char	*p;
-// 	size_t	idx;
+	idx = 0;
+	while (s[idx] != '\0')
+		idx++;
+	return (idx);
+}
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*p;
+	size_t	idx;
 
-// 	if (!s)
-// 		return (NULL);
-// 	idx = 0;
-// 	if (start >= ft_strlen(s))
-// 		len = 0;
-// 	else if (ft_strlen(s) - start < len)
-// 		len = ft_strlen(s) - start;
-// 	p = malloc(len + 1);
-// 	if (p == NULL)
-// 		return (NULL);
-// 	while (s[start + idx] != '\0' && idx < len)
-// 	{
-// 		p[idx] = s[start + idx];
-// 		idx++;
-// 	}
-// 	p[idx] = '\0';
-// 	return (p);
-// }
-// char	*ft_strdup(const char *s1)
-// {
-// 	int		s1_len;
-// 	int		idx;
-// 	char	*p;
+	if (!s)
+		return (NULL);
+	idx = 0;
+	if (start >= ft_strlen(s))
+		len = 0;
+	else if (ft_strlen(s) - start < len)
+		len = ft_strlen(s) - start;
+	p = malloc(len + 1);
+	if (p == NULL)
+		return (NULL);
+	while (s[start + idx] != '\0' && idx < len)
+	{
+		p[idx] = s[start + idx];
+		idx++;
+	}
+	p[idx] = '\0';
+	return (p);
+}
+char	*ft_strdup(const char *s1)
+{
+	int		s1_len;
+	int		idx;
+	char	*p;
 
-// 	s1_len = ft_strlen(s1);
-// 	p = malloc(s1_len + 1);
-// 	if (p == 0)
-// 		return (p);
-// 	idx = 0;
-// 	while (s1[idx] != '\0')
-// 	{
-// 		p[idx] = s1[idx];
-// 		idx++;
-// 	}
-// 	p[idx] = '\0';
-// 	return (p);
-// }
+	s1_len = ft_strlen(s1);
+	p = malloc(s1_len + 1);
+	if (p == 0)
+		return (p);
+	idx = 0;
+	while (s1[idx] != '\0')
+	{
+		p[idx] = s1[idx];
+		idx++;
+	}
+	p[idx] = '\0';
+	return (p);
+}
 
 int	count_space(char *str)
 {
@@ -79,14 +79,65 @@ int	count_space(char *str)
 			i++;
 			while (str[i] && str[i] != '"')
 				i++;
-			// if (str[i] != '"')
-			// 	Error;
 		}
 		else if (str[i] == ' ' && str[i + 1] && str[i + 1] != ' ')
 			space++;
 		i++;
 	}
 	return (space);
+}
+
+int	count_op_in_str(char *str)
+{
+	int	i;
+	int	op;
+
+	i = 0;
+	op = 0;
+	while (1)
+	{
+		if (str[i] == '<' || str[i] == '>')
+		{
+			if (i > 0)
+				op++;
+			if ((str[i] == '<' && str[i + 1] && str[i + 1] == '<')
+				|| (str[i] == '>' && str[i + 1] && str[i + 1] == '>'))
+				i++;
+			op++;
+		}
+		else if (!str[i])
+		{
+			op++;
+			break ;
+		}
+		i++;
+	}
+	return (op);
+}
+
+int	count_total_args(char *str)
+{
+	int	i;
+	int	args;
+	int	start_idx;
+
+	i = 0;
+	args = 0;
+	start_idx = 0;
+	while (1)
+	{
+		if (str[i] == ' ' || !str[i])
+		{
+			if (i > 0)
+				args += count_op_in_str(ft_substr(str, start_idx, i - start_idx));
+			while (str[i] == ' ')
+				i++;
+			if (!str[i])
+				break ;
+		}
+		i++;
+	}
+	return (args);
 }
 
 int	count_op(char **argv)
@@ -141,45 +192,48 @@ int	count_op(char **argv)
 // 	return (p);
 // }
 
-// void	divide_argv(char *command, char **argv)
-// {
-// 	int		i;
-// 	int		argv_idx;
-// 	int		start_idx;
-// 	char	*substr;
+char	**split_op(char *str)
+{
+	int		i;
+	int		split_idx;
+	int		start_idx;
+	char	**splited_op;
 
-// 	i = 0;
-// 	argv_idx = 0;
-// 	start_idx = 0;
-// 	while (1)
-// 	{
-// 		if (command[i] == '"')
-// 		{
-// 			i++;
-// 			while (command[i] != '"')
-// 				i++;
-// 		}
-// 		else if (command[i] == ' ' || !command[i])
-// 		{
-// 			substr = ft_substr_with_double_quotes(command, start_idx, i - start_idx);
-// 			argv[argv_idx] = substr;
-// 			argv_idx++;
-// 			if (command[i] == ' ')
-// 			{
-// 				while (command[i] == ' ')
-// 					i++;
-// 				start_idx = i;
-// 			}
-// 			// divide_op(substr, argv, argv_idx);
-// 			// operator도 분리
-// 			else if (!command[i])
-// 				break ;
-// 		}
-// 		i++;
-// 	}
-// 	argv[argv_idx] = NULL;
-// 	free(command);
-// }
+	i = 0;
+	split_idx = 0;
+	start_idx = 0;
+	while (str[i])
+	{
+		if (str[i] == '<')
+		{
+			if (i > 0)
+				splited_op[split_idx++] = ft_substr(str, start_idx, i - start_idx);
+			if (str[i + 1] && str[i + 1] == '<')
+			{
+				splited_op[split_idx++] = ft_substr(str, i, 2);
+				i++;
+			}
+			else
+				splited_op[split_idx++] = ft_substr(str, i, 1);
+			start_idx = i + 1;
+		}
+		else if (str[i] == '>')
+		{
+			if (i > 0)
+				splited_op[split_idx++] = ft_substr(str, start_idx, i - start_idx);
+			if (str[i + 1] && str[i + 1] == '>')
+			{
+				splited_op[split_idx++] = ft_substr(str, i, 2);
+				i++;
+			}
+			else
+				splited_op[split_idx++] = ft_substr(str, i, 1);
+			start_idx = i + 1;
+		}
+		i++;
+	}
+	return (splited_op);
+}
 
 char	**divide_argv(char *command)
 {
@@ -188,7 +242,7 @@ char	**divide_argv(char *command)
 	int		argv_idx;
 	char	**argv;
 
-	argv = malloc(sizeof(char *) * (count_space(command) + 1 + 1));
+	argv = malloc(sizeof(char *) * (count_total_args(command) + 1));
 	i = 0;
 	start_idx = 0;
 	argv_idx = 0;
@@ -196,7 +250,7 @@ char	**divide_argv(char *command)
 	{
 		if (!command[i] || command[i] == ' ')
 		{
-			argv[argv_idx++] = ft_substr(command, start_idx, i - start_idx);
+			split_op(ft_substr(command, start_idx, i - start_idx));
 			while (command[i] == ' ')
 				i++;
 			if (!command[i])
@@ -252,23 +306,31 @@ char	**extract_op(char **old_argv)
 	operator[op_idx] = NULL;
 	return (operator);
 }
-
-void	tokenize(t_token *tk, char *command)
+#include<stdio.h>
+// void	tokenize(t_token *tk, char *command)
+void	tokenize(char *command)
 {
 	int		i;
 	char	**old_argv;
 	char	**new_argv;
 	char	**operator;
 
-	// i = 0;
-	// while (command_line[i] != '|')
-	// 	i++;
-	// command = ft_substr(command_line, start, i);
-	old_argv = divide_argv(command);
-	new_argv = extract_new_argv(command, old_argv);
-	operator = extract_op(old_argv);
-	tk->argv = new_argv;
-	tk->operator = operator;
-	tk->cmd = tk->argv[0];
-	free_chars(old_argv);
+	printf("args:%d\n", count_total_args(command));
+	// old_argv = divide_argv(command);
+	// new_argv = extract_new_argv(command, old_argv);
+	// operator = extract_op(old_argv);
+	// tk->argv = new_argv;
+	// tk->operator = operator;
+	// tk->cmd = tk->argv[0];
+	// free_chars(old_argv);
+	// i = -1;
+	// while (old_argv[++i])
+	// 	printf("argv:%s\n", old_argv[i]);
+}
+
+int	main()
+{
+	char	*input = "echo hi>a";
+	printf("input:%s\n", input);
+	tokenize(input);
 }
