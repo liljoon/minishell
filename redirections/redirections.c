b6eae10ca6dfec91542c8009f31c81f4c34ea5e3
Iljoon Sunwoo <isunwoo@c6r10s6.42seoul.kr>
2023-03-11 21:01:28 +0900

    refactor : separate heredoc file

--- redirections/redirections.c
+++ redirections/redirections.c
@@ -6,11 +6,11 @@
 /*   By: isunwoo <isunwoo@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 12:11:58 by isunwoo           #+#    #+#             */
-/*   Updated: 2023/03/10 13:25:37 by isunwoo          ###   ########.fr       */
+/*   Updated: 2023/03/11 20:44:16 by isunwoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "redirections.h"
 
 void	redirection_output(char *file)
 {
@@ -43,57 +43,6 @@ void	redirection_append(char *file)
 	fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
 	dup2(fd, 1);
 	close(fd);
-}
-
-void	redirection_heredoc(char *arg)
-{
-	int		temp_fd;
-	char	*command;
-
-	temp_fd = open("./.heredoc_temp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	while (1)
-	{
-		command = readline("> ");
-		if (!command)
-			break ;
-		else if (ft_strncmp(arg, command, ft_strlen(arg)) == 0)
-		{
-			free(command);
-			break ;
-		}
-		write(temp_fd, command, ft_strlen(command));
-		write(temp_fd, "\n", 1);
-		free(command);
-	}
-	close(temp_fd);
-
-}
-
-void	check_heredoc_first(t_token *tk)
-{
-	char	**operator;
-	int		flag;
-	int		fd;
-
-	flag = 0;
-	operator = tk->operator;
-	while (*operator)
-	{
-		if (ft_strncmp(*operator, "<<", 3) == 0)
-		{
-			redirection_heredoc(*(operator + 1));
-			flag = 1;
-		}
-		else
-			operator--;
-		operator += 2;
-	}
-	if (flag)
-	{
-		fd = open("./.heredoc_temp", O_RDONLY);
-		dup2(fd, 0);
-		close(fd);
-	}
 }
 
 int	check_redirections(t_token *tk)
---
+++ redirections/redirections.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirections.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isunwoo <isunwoo@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/11 20:42:48 by isunwoo           #+#    #+#             */
+/*   Updated: 2023/03/11 20:44:32 by isunwoo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REDIRECTIONS_H
+# define REDIRECTIONS_H
+# include "../minishell.h"
+
+void	check_heredoc_first(t_token *tk);
+
+#endif
