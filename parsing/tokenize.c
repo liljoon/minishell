5f512ff0d847dfe2f783f96675320616f46f00be
YoonJu Ham <judyham10634@naver.com>
2023-03-13 16:51:27 +0900

    chore: delete comments

--- parsing/tokenize.c
+++ parsing/tokenize.c
@@ -6,13 +6,12 @@
 /*   By: yham <yham@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 16:51:25 by yham              #+#    #+#             */
-/*   Updated: 2023/03/11 20:47:22 by yham             ###   ########.fr       */
+/*   Updated: 2023/03/13 16:50:38 by yham             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-// void	tokenize(char *command)
 t_token	*tokenize(char *command)
 {
 	int		i;
@@ -24,7 +23,6 @@ t_token	*tokenize(char *command)
 	if (check_exceptions(command))
 		return (NULL);
 	tk = malloc(sizeof(t_token));
-	// printf("args:%d\n", count_total_args(command));
 	old_argv = divide_argv(command);
 	new_argv = extract_new_argv(command, old_argv);
 	operator = extract_op(old_argv);
@@ -32,15 +30,6 @@ t_token	*tokenize(char *command)
 	tk->operator = operator;
 	tk->cmd = tk->argv[0];
 	tk->next = NULL;
-	// i = -1;
-	// while (old_argv[++i])
-	// 	printf("old:%s\n", old_argv[i]);
-	// i = -1;
-	// while (new_argv[++i])
-	// 	printf("new:%s\n", new_argv[i]);
-	// i = -1;
-	// while (operator[++i])
-	// 	printf("op:%s\n", operator[i]);
 	free_chars(old_argv);
 	return (tk);
 }
