01edaeef9784a55c49a2f67862ea7547781e52c7
Iljoon Sunwoo <isunwoo@c6r10s6.42seoul.kr>
2023-03-24 16:47:47 +0900

    fix : SIGQUIT cursor error

--- sig_handle.c
+++ sig_handle.c
@@ -6,7 +6,7 @@
 /*   By: isunwoo <isunwoo@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 14:00:02 by isunwoo           #+#    #+#             */
-/*   Updated: 2023/03/17 18:47:16 by isunwoo          ###   ########.fr       */
+/*   Updated: 2023/03/24 16:47:13 by isunwoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ void	sigquit_handler(int signo)
 {
 	if (signo != SIGQUIT)
 		return ;
+	rl_redisplay();
 	return ;
 }
 
